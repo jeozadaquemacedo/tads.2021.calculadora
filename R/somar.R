5f7b4148253627cc0461c03c17fd955a06ac1bab
Jeozadaque Macedo <jam6@discente.ifpe.edu.br>
2021-11-06 11:56:36 -0300

    my fisrt commit

---
+++ R/somar.R
@@ -0,0 +1,13 @@
+#' Função para somar
+#'
+#' essa função serve para somar dois números
+#'
+#' @param a um número
+#' @param b outro número
+#'
+#' @examples
+#' somar (4, 2)
+#' @export
+somar <- function(a, b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,16 @@
+#' Função para subtrair
+#'
+#' essa função serve para subtrair dois números
+#'
+#' @param a um número
+#' @param b outro número
+#'
+#' @examples
+#' subtrair (6, 2)
+
+
+
+#' @export
+subtrair <- function(a,b){
+  a - b
+}
